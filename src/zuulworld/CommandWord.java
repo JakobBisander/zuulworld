88de00e4ecaf8c547e1e158f4f1ea634d6da3f78
Jabadjuni <jakob.bisander@hotmail.com>
2016-10-10 09:46:40 +0200

    Sikkerhed and stuff

--- src/zuulworld/CommandWord.java
+++ src/zuulworld/CommandWord.java
@@ -4,7 +4,7 @@ package zuulworld;
 
 public enum CommandWord
 {
-    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), ATTACK("attack");
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), ATTACK("attack"), FLEE("flee");
     
     private final String commandString;
     
